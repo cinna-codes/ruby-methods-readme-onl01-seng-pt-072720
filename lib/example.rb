a29c0ca7c52fd03fe6305fcbee543c8894921d61
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-07-28 18:18:21 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -3,16 +3,8 @@
 phrase = "Hello World!"
 
 def say_hello_ten_times
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
-
 say_hello_ten_times
